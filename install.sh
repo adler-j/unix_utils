8ad43e0b57ec321732e20e3eea76c777b569cea4
Jonas Adler <jonasadl@kth.se>
2015-11-27 10:46:03 +0100

    Update and rename vim_install to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+# install bash
+curl -o .bashrc https://raw.githubusercontent.com/adler-j/unix_utils/master/.bashrc
+
+#vim
 sudo apt-get install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+#clang format
 sudo apt-get install clang-format-3.5
 git clone https://github.com/rhysd/vim-clang-format ~/.vim/bundle/vim-clang-format
 
@@ -15,3 +20,5 @@ vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
+
+curl -o .bashrc https://raw.githubusercontent.com/adler-j/unix_utils/master/.bashrc
